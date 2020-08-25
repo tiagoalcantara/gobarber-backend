dadb5baada03e48c7b4b83e7330e94f7c5e5dd0d
Tiago Alcantara <tiagofacampos@gmail.com>
2020-08-25 12:52:16 -0300

    Initial commit

---
+++ src/services/CreateAppointmentService.ts
@@ -0,0 +1,36 @@
+import { startOfHour } from 'date-fns';
+import { getCustomRepository } from 'typeorm';
+import Appointment from '../models/Appointment';
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+
+interface RequestDTO {
+  providerId: string;
+  date: Date;
+}
+
+class CreateAppointmentService {
+  public async execute({ providerId, date }: RequestDTO): Promise<Appointment> {
+    const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+
+    const appointmentDate = startOfHour(date);
+
+    const findAppointmentInSameDate = await appointmentsRepository.findByDate(
+      appointmentDate,
+    );
+
+    if (findAppointmentInSameDate) {
+      throw Error('This appointment is already booked');
+    }
+
+    const appointment = appointmentsRepository.create({
+      providerId,
+      date: appointmentDate,
+    });
+
+    await appointmentsRepository.save(appointment);
+
+    return appointment;
+  }
+}
+
+export default CreateAppointmentService;
